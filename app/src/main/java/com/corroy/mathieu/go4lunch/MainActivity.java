1cfaf3bd5eaf13ef932cf5be41e34b56c8a707a2
Mathieu72000 <Math.corroy@gmail.com>
2019-04-01 17:06:37 +0200

    Add new activity : - FirstScreenActivity (First Screen after connexion) with his layout
    MainActivity : Completed the connexion screen with 2 buttons :
    - Google
    - Facebook
    -Configure those buttons with FireBase methods
    - Add a new SnackBar
    
    Add a new method who handle response after sign-in activity
    
    Add new Drawables
    Add new Colors
    Add new Strings
    
    Add new Translations

--- app/src/main/java/com/corroy/mathieu/go4lunch/MainActivity.java
+++ app/src/main/java/com/corroy/mathieu/go4lunch/MainActivity.java
@@ -1,13 +1,108 @@
 package com.corroy.mathieu.go4lunch;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getFragmentLayout() {
+        return R.layout.activity_main;
+    }
+
+    // FOR DESIGN
+    // - Get Coordinator Layout
+    @BindView(R.id.mainActivity_CoordinatorLayout)
+    CoordinatorLayout coordinatorLayout;
+
+    //FOR DATA
+    // - Identifier for Sign-In Activity
+    private static final int RC_SIGN_IN = 123;
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+
+    @OnClick(R.id.mainActivity_google_login)
+    public void onClickGoogleButton(){this.startSignInActivityForGoogle(); }
+    @OnClick(R.id.mainActivity_facebook_login)
+    public void onClickFacebookButton(){this.startSignInActivityForFacebook();}
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // - Handle SignIn Activity response on activity result
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    // --------------------
+    // NAVIGATION
+    // --------------------
+
+    // - Launch Sign-In Activity for Google
+    private void startSignInActivityForGoogle(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build())) //GOOGLE
+                        .setIsSmartLockEnabled(false, true)
+                        .build(), RC_SIGN_IN);
+    }
+
+    // - Launch Sign-In Activity for Facebook
+    private void startSignInActivityForFacebook(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.FacebookBuilder().build())) //GOOGLE
+                        .setIsSmartLockEnabled(false, true)
+                        .build(), RC_SIGN_IN);
+    }
+
+    // --------------------
+    // UI
+    // --------------------
+
+    // - Show Snack Bar with a message
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    // --------------------
+    // UTILS
+    // --------------------
+
+    // - Method that handles response after SignIn Activity close
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                showSnackBar(this.coordinatorLayout, "Connexion Success !");
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, "Authentification Canceled");
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, "No Network");
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, "Unknown Error");
+                }
+            }
+        }
     }
 }
